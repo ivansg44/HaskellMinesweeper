792bd1f442c76ffa6765464a8623d7b303e81671
Ivan S Gill <isgill93@student.ubc.ca>
2021-02-16 23:56:09 -0800

    wip

--- HaskellMinesweeper.hs
+++ HaskellMinesweeper.hs
@@ -2,9 +2,9 @@
 module HaskellMinesweeper where
 
 data State = State [Coordinate] -- Uncovered cells
-                   [Coordinate] -- Flagged cells
-                   [Coordinate] -- Mines
-                   Int          -- # of cells in one row and one col
+                   [Coordinate] -- Unflagged covered cells
+                   [Coordinate] -- Flagged covered cells
+                   [Coordinate] -- Mines (can overlap with flagged cells)
   deriving (Show)
 
 data Result = EndOfGame Bool               -- True means victory
@@ -15,44 +15,62 @@ data Result = EndOfGame Bool               -- True means victory
 
 type Game = Action -> State -> Result
 
-data Action = Flag Coordinate    -- Flag a cell
+data Action = Flag Coordinate    -- Flag or unflag a cell
             | Uncover Coordinate -- Uncover a cell
 
 -- Cell x y coordinates
 type Coordinate = (Int, Int)
 
 haskellminesweeper :: Game
-haskellminesweeper _ (State _ _ _ _) = EndOfGame False
+haskellminesweeper (Uncover (x,y)) (State uncovered unflagged flagged mines)
+ | elem (x,y) mines                  = EndOfGame False
+ | [(x,y)] == (unflagged ++ flagged) = EndOfGame True
+ | otherwise                         = ContinueAfterClear 0 (State [] [] [] []) -- TODO finish
+haskellminesweeper (Flag (x,y)) (State uncovered unflagged flagged mines)
+ | elem (x,y) flagged =
+    ContinueAfterFlag (State uncovered
+                             ((x,y):unflagged)
+                             [e | e <- flagged, e /= (x,y)]
+                             mines)
+ | otherwise          =
+    ContinueAfterFlag (State uncovered
+                             [e | e <- unflagged, e /= (x,y)]
+                             ((x,y):flagged)
+                             mines)
 
 {--
 Test cases
 
-TODO we can more explicitly reject illegal moves in IO
-TODO I made it one-based index because it may be easier to compute
+TODO we can reject illegal moves in IO
 
-haskellminesweeper (Uncover (2,1)) (State [(1,1),(1,2)] [] [(2,1)] 2)
+haskellminesweeper (Uncover (1,0)) (State [(0,0),(0,1)] [(1,1)] [] [(1,0)])
 == EndOfGame False
 
-haskellminesweeper (Uncover (2,2)) (State [(1,1),(1,2)] [] [(2,1)] 2)
+haskellminesweeper (Uncover (1,1)) (State [(0,0),(0,1)] [(1,1)] [] [(1,0)])
 == EndOfGame True
 
-haskellminesweeper (Flag (2,2)) (State [(1,1),(1,2)] [] [(2,1)] 2)
-== ContinueAfterFlag (State [(1,1),(1,2)] [(2,2)] [(2,1)] 2)
+haskellminesweeper (Flag (1,0)) (State [(0,0),(0,1)] [(1,1)] [] [(1,0)])
+== ContinueAfterFlag (State [(0,0),(0,1)] [(1,1)] [(1,0)] [(1,0)]
 
-haskellminesweeper (Uncover (2,2)) (State [(1,1),(1,2)] [(2,2)] [(2,1)] 2)
+haskellminesweeper (Flag (1,1)) (State [(0,0),(0,1)] [(1,1)] [] [(1,0)])
+== ContinueAfterFlag (State [(0,0),(0,1)] [] [(1,1)] [(1,0)])
+
+haskellminesweeper (Flag (1,1)) (State [(0,0),(0,1)] [] [(1,1)] [(1,0)])
+== ContinueAfterFlag (State [(0,0),(0,1)] [(1,1)] [] [(1,0)])
+
+haskellminesweeper (Uncover (1,1)) (State [(0,0),(0,1)] [] [(1,1)] [(1,0)])
 == EndOfGame True
 
-haskellminesweeper (Uncover (10,10)) (State [(1,1),(1,2)] [] [(2,1)] 10)
-== ContinueAfterClear 0 (State [(10,10),(1,1),(1,2)] [] [(2,1)] 10)
+haskellminesweeper (Uncover (2,2)) (State [(0,1)] [(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)] [] [(0,0)])
+== ContinueAfterClear 0 (State [(2,2),(0,1)] [(0,2),(1,0),(1,1),(1,2),(2,0),(2,1)] [] [(0,0)])
 
-haskellminesweeper (Uncover (1,1)) (State [] [] [(2,2)] 3)
-== ContinueAfterClear 1 (State [(1,1)] [] [(2,2)] 3)
+haskellminesweeper (Uncover (1,1)) (State [(0,1)] [(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)] [] [(0,0)])
+== ContinueAfterClear 1 (State [(1,1),(0,1)] [(0,2),(1,0),(1,2),(2,0),(2,1),(2,2)] [] [(0,0)])
 
-haskellminesweeper (Uncover (2,2)) (State [] [] [(1,1),(3,3)] 3)
-== ContinueAfterClear 2 (State [(2,2)] [] [(1,1),(3,3)] 3)
+haskellminesweeper (Uncover (1,1)) (State [(0,1)] [(0,2),(1,1),(2,0),(2,1),(2,2)] [] [(0,0),(1,0),(1,2)])
+== ContinueAfterClear 3 (State [(1,1),(0,1)] [(0,2),(2,0),(2,1),(2,2)] [] [(0,0),(1,0),(1,2)])
 
-haskellminesweeper (Uncover (2,2)) (State [] [] [(1,1),(1,2),(1,3),(2,1),(2,3),(3,1),(3,2),(3,3)] 3)
-== ContinueAfterClear 8 (State [(2,2)] [] [(1,1),(1,2),(1,3),(2,1),(2,3),(3,1),(3,2),(3,3)] 3)
-
+haskellminesweeper (Uncover (1,1) (State [] [(0,1),(1,1)] [] [(0,0),(0,2),(1,0),(1,2),(2,0),(2,1),(2,2)])
+== ContinueAfterClear 7 (State [(1,1)] [(0,1)] [] [(0,0),(0,2),(1,0),(1,2),(2,0),(2,1),(2,2)])
 --}
 
