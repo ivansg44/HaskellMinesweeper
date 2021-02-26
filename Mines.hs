68af69165034f59917edd2f46fb408c408eacf16
Dennis Truong <dennisnhatsontruong@gmail.com>
2021-02-26 00:37:15 -0800

    clean up and comments mines and user init

--- Mines.hs
+++ Mines.hs
@@ -3,30 +3,33 @@ import System.Random
 import HaskellMinesweeper
 import Data.List
     
-    -- Generates a list of tuple coordinates of the entire grid of size n 
+
+
+-- Generates a list of tuple coordinates of the entire grid of size n 
 gridLocations :: Int -> [Coordinate]
 gridLocations n = [(x,y) | x <- [0..n-1], y <- [0..n-1]]
 
--- Genertes random list of numbers that lie within 0-total grid squares
+
+-- Generates random list of indices numbers that fit inside the grid
 randomIndices :: Int                -- gridSize
                -> Int               -- number of Indexes (mines)
                -> IO [Int]          -- list of indexes
 randomIndices _ 0 = return []
 randomIndices gridSize num =
     do
-        g <- newStdGen                                                      -- generate random "seed"
-        let infList = randomRs (0, gridSize*gridSize-1) g                   -- randomRs create infinite list given a "seed" value g,
-        return (take num (nub infList))                                     -- nub removes duplicates, then take the necessary amount
+        seed <- newStdGen                                               -- generate random "seed"
+        let infList = randomRs (0, gridSize*gridSize-1) seed            -- randomRs create infinite list given a "seed" value g,
+        return (take num (nub infList))                                 -- nub removes duplicates, then take the necessary amount
                                                                             
 
 
--- Returns gridLocations at indicated indexes
+-- Returns gridLocations at indicated indices
 returnBombLocation :: Int              -- Counter to make sure we reached end of list
                   -> [Int]             -- List of Indexes to check
                   -> [Coordinate]      -- List of gridLocations to check
                   -> [Coordinate]      -- Outputed gridLocations at said indexes
-returnBombLocation counter indexes positions
-    | counter /= length indexes = positions !! (indexes !! counter) : returnBombLocation (counter+1) indexes positions
+returnBombLocation counter indices grid
+    | counter < length indices = grid !! (indices !! counter) : returnBombLocation (counter+1) indices grid
     | otherwise = []        
 
 
@@ -37,39 +40,21 @@ randomBombLocations :: Int             -- gridSize
                   -> IO [Coordinate]   -- list of bomb locations
 randomBombLocations gridSize num = 
     do
-        indexes <- randomIndices gridSize num
-        return $ returnBombLocation 0 indexes $ gridLocations gridSize
+        indices <- randomIndices gridSize num
+        return (returnBombLocation 0 indices (gridLocations gridSize))
 
 
-
--- Tests
-
--- exampleGrid = gridLocations 4
--- == [(0,0),(0,1),(0,2),(0,3),(1,0),(1,1),(1,2),(1,3),(2,0),(2,1),(2,2),(2,3),(3,0),(3,1),(3,2),(3,3)]
-
--- indices = randomIndices 4 4
--- indices == [different every time]
-
--- bombLocations = returnBombLocation  0 [0, 10, 1, 14] exampleGrid
--- [(0,0),(2,2),(0,1),(3,2)]
-
--- exampleRandomBombs = randomBombLocations 10 10 
--- exampleRandomBombs = [different every time]
-
-------------------------------------------------------------------------------------------------------
--- Essentially bomb generation code,
-initialState :: Int            
-                  -> Int               
-                  -> IO State  
+-- Creates initial game state of 4 [Coordinate]
+-- Uncovered == []
+-- Covered == All cells not mines
+-- Flagged == []
+-- Mines == Generated random mines
+initialState :: Int                 --gridSize      
+                  -> Int            --mine amount   
+                  -> IO State       --Starting state
 initialState gridSize num = 
     do
-        indices <- randomIndices gridSize num
         mines <- randomBombLocations gridSize num
         let covered = gridLocations gridSize \\ mines
         return (State [] covered [] mines)
 
-
-
--- Test
---  exampleStart = initialState 3 3
--- savedStart = exampleStart
--- UserInitialization.hs
+++ UserInitialization.hs
@@ -4,9 +4,12 @@ import HaskellMinesweeper
 import Mines
 import Text.Read (readMaybe)
 
+
+-- User is prompted to enter an Int greater than 0 to set grid size
+-- Repeats if given an invalid response
 userGridSize :: IO Int
 userGridSize = do
-       putStrLn "Enter single positive integer (>0) for grid length and width"
+       putStrLn "Enter single positive integer (> 0) for grid length and width"
        line <- getLine
        case (readMaybe line :: Maybe Int) of 
            Nothing ->
@@ -17,14 +20,16 @@ userGridSize = do
                 else
                     return size
 
-
+-- Returns true if given a 0 or negative number (use to get valid grid size)
 zeroOrNegGrid :: Int -> Bool
 zeroOrNegGrid num = num <= 0
 
 
+-- Prompts user to enter an integer to dtermine the numer of mines they would like to have
+-- Repeats if given an invalid response
 userMineCount :: Int -> IO Int
 userMineCount board = do
-       putStrLn ("Enter number of mines greater than 0 and  less than "  ++  show (board*board))
+       putStrLn ("Enter number of mines greater than 0 and less than "  ++  show (board*board))
        line <- getLine
        case (readMaybe line :: Maybe Int) of 
            Nothing ->
@@ -35,6 +40,9 @@ userMineCount board = do
                 else
                     return mines
 
+
+-- Returns true if given an invalid number of mines
+-- invalid mines are negative or greater than the number of squares
 invalidMineCount :: Int -> Int -> Bool
 invalidMineCount mines gridSize = (mines < 0) || (mines >= gridSize*gridSize)
 
