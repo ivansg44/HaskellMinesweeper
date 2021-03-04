dcc1d484311f1655ada70bc5e2db2bb4418c7b76
Ivan S Gill <isgill93@student.ubc.ca>
2021-03-04 10:27:20 -0800

    In-code doc

--- AskForAction.hs
+++ AskForAction.hs
@@ -1,27 +1,39 @@
--- TODO document
+-- This module handles user input.
 module AskForAction where
 
 import HaskellMinesweeper
 
 import Text.Read (readMaybe)
 
-askforaction :: State -> Int -> IO Action
+-- Entry point function that asks users for input, and eventually returns
+-- action.
+askforaction :: State     -- Current state of game
+             -> Int       -- Size of board
+             -> IO Action -- Returns action or branches to functions for error
+                          -- handling.
 askforaction state size =
  do
   putStrLn "What move would you like to make?"
   line <- getLine
   case (readMaybe line :: Maybe Action) of
    Nothing ->
+    -- Invalid action
     invalidactionmsg state size
    Just action ->
+    -- Invalid range of coordinates in action
     if invalidrange (getcoords action) size then
      invalidrangemsg state size
+    -- Tried to flag uncovered cell
     else if invalidflag action state then
      invalidflagmsg state size
     else
+     -- All good
      return action
 
-invalidactionmsg :: State -> Int -> IO Action
+-- Tells user they have inputted an invalid action
+invalidactionmsg :: State     -- Current state of game
+                 -> Int       -- Size of board
+                 -> IO Action -- Branches back to askforaction
 invalidactionmsg state size =
  do
   putStrLn "Please use the following formats:"
@@ -30,30 +42,36 @@ invalidactionmsg state size =
   putStrLn ("...where row and column are integers less than " ++ (show size))
   askforaction state size
 
--- invalidrange :: Coordinate -> Int -> Bool
--- invalidrange (x,y) size = (x >= size) && (y >= size)
-invalidrange :: Coordinate -> Int -> Bool
+-- Checks if a coordinate is invalid given size of board
+invalidrange :: Coordinate -- Coordinate to check
+             -> Int        -- Size of board
+             -> Bool       -- Returns true if invalid coordinate
 invalidrange (x,y) size = (x >= size) || (y >= size) || (x < 0) || (y < 0)
 
-invalidrangemsg :: State -> Int -> IO Action
+-- Tells user they have inputted an action with an invalid range
+invalidrangemsg :: State     -- Current state of game
+                -> Int       -- Size of board
+                -> IO Action -- Branches back to askforaction
 invalidrangemsg state size =
  do
   putStrLn "Out of bounds."
   putStrLn ("row and column must be integers less than " ++ (show size))
   askforaction state size
 
-invalidflag :: Action -> State -> Bool
+-- Checks if user tried to flag uncovered cell
+invalidflag :: Action -- Action by user
+            -> State  -- Current state of game
+            -> Bool   -- Returns true if user tried to flag uncovered cell
 invalidflag (Flag coord) (State uncovered _ _ _) = elem coord uncovered
+-- User did not flag a cell
 invalidflag _ _ = False
 
-invalidflagmsg :: State -> Int -> IO Action
+-- Tells user they have tried to flag an uncovered cell
+invalidflagmsg :: State     -- Current state of game
+               -> Int       -- Size of board
+               -> IO Action -- Branches back to askforaction
 invalidflagmsg state size =
  do
   putStrLn "You cannot flag an uncovered cell."
   askforaction state size
-
-{--
-Test cases TODO
-
---}
 
--- PrintBoard.hs
+++ PrintBoard.hs
@@ -1,26 +1,33 @@
---TODO document
---idea: in IO we start with printinitboard, then when user does something, we 
---      use information from their action and the result to printnewboard. We 
---      need to keep track of the old board string in IO. TODO
+-- Prints a board representing the game.
+-- This works independent of the game framework. We must modify both the game
+-- and a string representation of the board in Start.hs.
 module PrintBoard where
 
 import HaskellMinesweeper
 import Mines
 
-printinitboard :: Int -> [Char]
+-- Print initial board.
+-- Gets a list of identical rows from printinitboardrows, and then concatenates
+-- them into a single string with a '\n' delimiter.
+printinitboard :: Int    -- Size of initial board
+               -> [Char] -- String representation of initial board
 printinitboard size = foldl (\ acc e -> acc ++ ['\n'] ++ e) h t
  where (h:t) = printinitboardrows size
 
-printinitboardrows :: Int -> [[Char]]
+-- Get a list of rows in the initial board
+printinitboardrows :: Int      -- Size of init board i.e., how many rows needed
+                   -> [[Char]] -- List of identical rows that will be
+                               -- concatenated into a single string in
+                               -- printinitboard.
 printinitboardrows size = [printinitboardrow size | _ <- [1..size]]
 
-printinitboardrow :: Int -> [Char]
+-- Get a single row in initial board
+printinitboardrow :: Int   -- Size of init board i.e., how long row should be
+                 -> [Char] -- "-----"... as long as needed
 printinitboardrow size = ['-' | _ <- [1..size]]
 
 {--
 Test cases
-
--- TODO will not work with 0
 
 putStrLn(printinitboard 1)
 ==
@@ -38,15 +45,27 @@ putStrLn(printinitboard 3)
 
 --}
 
-printnewboard :: Coordinate -> Char -> [Char] -> [Char]
+-- Updates existing board string at specific coordinate with new character
+printnewboard :: Coordinate -- Coordinate of board string to update
+              -> Char       -- Character to place at that coordinate
+              -> [Char]     -- Old board string
+              -> [Char]     -- Updated board string
 printnewboard coord newval oldboard =
  printnewboardhelper coord newval oldboard 0 0
 
+-- Even though the string does not have any built-in coordinates, we pretend it
+-- does, by imagining the string is split into row by the '\n' character, and
+-- the characters in each row belong to a different column. This helper uses
+-- two Int values as x and y coordinate accumulators, until it reaches the
+-- coordinate to modify.
 printnewboardhelper :: Coordinate -> Char -> [Char] -> Int -> Int -> [Char]
 printnewboardhelper _ _ [] _ _ = []
 printnewboardhelper coord newval (h:t) xacc yacc
+ -- This is the coordinate. Replace it.
  | coord == (xacc,yacc) = newval:t
+ -- This is not the coordinate, but move to a new row.
  | h == '\n'            = h:(printnewboardhelper coord newval t (xacc+1) 0)
+ -- This is not the coordinate. Move to the next column in this row.
  | otherwise            = h:(printnewboardhelper coord newval t xacc (yacc+1))
 
 {---
